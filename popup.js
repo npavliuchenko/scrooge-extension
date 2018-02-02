ba10d41591102ff279f2f1ffdf418a77bb6a5a05
npavliuchenko <npavliuchenko@terricone.com>
2018-02-02 12:07:01 +0200

    initial commit. Basic extensions, shows popup with console.log

---
+++ popup.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var grabButton = document.getElementById('grabContents');
+  grabButton.addEventListener('click', function() {
+    console.log('woo-hoo!');
+  });
+});
